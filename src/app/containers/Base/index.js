ee10cf6fb637504e8907224ca1894c54064b3095
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-04 20:16:36 -0300

    Criando o menu da  DashBoard 2/3

--- src/app/containers/Base/index.js
+++ src/app/containers/Base/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 
-const BarraTopo = () => (
-  <div className="flex horizontal full-width">
-    <div className="flex-1 flex flex-start">
-      <p>Ver Loja</p>
-    </div>
-    <div className="flex-1 flex flex-end">
-      <p>Sair</p>
-    </div>
-  </div>
-)
-
+import BarraTopo from './BarraTopo';
+import Menu from './Menu';
 
 class DashBoard extends React.Component {
   render(){
     return (
       <div className="flex horizontal full-height">
         <div className="flex vertical">
-          <p>Menu</p>
+          <Menu history={this.props.history}/>
         </div>
         <div className="flex vertical full-width">
           <div className="flex horizontal">
-           <BarraTopo />
+            <BarraTopo />
           </div>
           <div className="flex full-height">
             { this.props.children }
