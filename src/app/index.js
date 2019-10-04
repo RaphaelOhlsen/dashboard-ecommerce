6c38192f1fa6b824fdf580b3b7844a63d7098f3b
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-04 17:18:40 -0300

    Instalando o sistema de Rotas e Criando o primeiro COntainer e Componente

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        Dashboard
-      </div>
-    </Provider>
-  );
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Pedidos from './containers/Pedidos/';
+
+class  App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route path={"/"} exact component={ Pedidos } />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+  
 }
 
 export default App;
