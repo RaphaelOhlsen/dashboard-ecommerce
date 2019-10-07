a10501209621d0de5c3f64604028306ecc8b8458
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-07 16:44:47 -0300

    Tela de Detalhes do Pedido - Finalizando a parte dos Dados dos Pedidos

--- src/app/containers/Pedido/DetalhesDoPedido.js
+++ src/app/containers/Pedido/DetalhesDoPedido.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Titulo from '../../components/Texto/Titulo'
 import Button from '../../components/Button/Simples';
+import { TextoDados } from '../../components/Texto/Dados';
+import Tabela from '../../components/Tabela/Simples';
 
 class DetalhesDoPedido extends Component {
 
@@ -17,7 +19,74 @@ class DetalhesDoPedido extends Component {
                   onClick={() => alert('Cancelado')} 
           />
         </div>
-        
+      </div>
+    )
+  }
+
+  renderDadosDoCliente(){
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados do Cliente" />
+        <br/>
+        <TextoDados chave="Nome" valor="Cliente 1" />
+        <TextoDados chave="CPF" valor="009.599.147-65" />
+        <TextoDados chave="Telefone" valor="27 99876-9065" />
+        <TextoDados chave="Data de Nascimento" valor="10/04/1987" />
+      </div>
+    )
+  }
+
+  renderDadosDeEntrega(){
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados de Entrega" />
+        <br/>
+        <TextoDados chave="Endereco" valor="Rua TEste 123," />
+        <TextoDados chave="Bairro" valor="Centro" />
+        <TextoDados chave="Cidade" valor="Uberaba" />
+        <TextoDados chave="Estado" valor="Minas Gerais" />
+        <TextoDados chave="CEP" valor="38540-123" />
+      </div>
+    )
+  }
+
+  renderDadosDePagamento(){
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados de Pagamento" />
+        <br/>
+        <TextoDados chave="Taxa de Entrega" valor="R$ 15,50 (PAC)" />
+        <TextoDados chave="Valor do Pedido" valor="R$ 32,00" />
+        <TextoDados chave="Valor Total" valor="R$ 47,50" />
+        <TextoDados chave="Forma de Pagamento" valor="Boleto" />
+      </div>
+    )
+  }
+
+  renderDadosDoCarrinho(){
+    const dados = [
+      {
+        "Produto": "Produto 1",
+        "Preço Und.": "R$ 12,00",
+        "Quantidade": "1",
+        "Preço Total": "R$ 12,00"
+      },
+      {
+        "Produto": "Produto 2",
+        "Preço Und.": "R$ 10,00",
+        "Quantidade": "2",
+        "Preço Total": "R$ 20,00"
+      }
+    ];
+
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Carrinho" />
+        <br/>
+        <Tabela 
+          cabecalho={["Produto", "Preço Uni.", "Quantidade", "Preço Total"]}
+          dados={ dados }
+        />
       </div>
     )
   }
@@ -26,6 +95,10 @@ class DetalhesDoPedido extends Component {
     return (
       <div className="Detalhes-do-Pedido">
         { this.renderCabecalho() }
+        { this.renderDadosDoCliente() }
+        { this.renderDadosDoCarrinho() }
+        { this.renderDadosDeEntrega() }
+        { this.renderDadosDePagamento() }
       </div>
     )
   }
