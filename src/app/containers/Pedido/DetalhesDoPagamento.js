0ffccb87708b0c7601116d41b4ec5a6a10364014
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-07 18:44:10 -0300

    Tela de Detalhes do Pedido - Criando a estrutura para detalhers/status do pagamento - 3/6

--- src/app/containers/Pedido/DetalhesDoPagamento.js
+++ src/app/containers/Pedido/DetalhesDoPagamento.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+import Titulo from '../../components/Texto/Titulo';
+import ListaDinamica from '../../components/Listas/ListaDinamicaSimples';
+
 class DetalhesDoPagamento extends Component {
+  state = {
+    status: [
+      "Aguardando Pagamento",
+      "Processando Pagamento",
+    ]
+  }
+
+  onRemoveListaDinamica = (index) => {
+    let { status } = this.state;
+    status = status.filter((item, _index) => _index !== index );
+    this.setState = ({ status });
+  }
+
+  onAddListaDinamica = (texto) => {
+    if (!texto) return false;
+    let { status } = this.state;
+    status.push(texto);
+    this.setState({ status})
+  }
+
   render(){
+    const { status } = this.state;
     return (
       <div className="Detalhes-do-Pagamento">
-        DetalhesDoPagamento
+        <Titulo tipo="h4" titulo="Pagamento" />
+        <br/>
+        <ListaDinamica 
+          dados={ status }
+          onRemove={this.onRemoveListaDinamica}
+          onAdd={this.onnAddistaDinamica}
+        />
       </div>
     )
   }
