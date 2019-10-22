243817d04289d8fd6c23efb9f27cf3b2956f8d27
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-22 16:00:06 -0300

    Login com Erro: Criando Error Hangling 2/2

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -6,57 +6,15 @@ import {
 } from './types';
 
 import { api, versao } from '../config';
-
-//LOCAL STORAGE
-const saveToken = (usuario, opcaoLembrar) => {
-  if(!usuario.token) return null;
-  const [ token1, token2, token3 ] = usuario.token.split('.');
-  localStorage.setItem("token1", token1);
-  localStorage.setItem("token2", token2);
-  localStorage.setItem("token3", token3);
-  localStorage.setItem('opcaoLembrar', opcaoLembrar);
-}
-
-const cleanToken = () => {
-  localStorage.removeItem('token1');
-  localStorage.removeItem('token2');
-  localStorage.removeItem('token3');
-  localStorage.removeItem('opcaoLembrar');
-}
-
-const getToken = () => {
-  const token1 = localStorage.getItem('token1');
-  const token2 = localStorage.getItem('token2');
-  const token3 = localStorage.getItem('token3');
-  if(!token1 || !token2 || !token3) return null;
-  return `${token1}.${token2}.${token3}`;
-}
-
-const getHeaders = () => {
-  return {
-    "headers": {
-      "authorization": `Ecommerce ${getToken()}`
-    }
-  }
-}
+import { cleanToken, saveToken, getHeaders } from './localStorage';
+import errorHandling from './errorHandling';
 
 export const initApp = () => {
   const opcaoLembrar = localStorage.getItem('opcaoLembrar');
   if(opcaoLembrar === 'false') cleanToken();
 }
 
-// ERROR HANDLING
-const errorHandling = error => {
-  if(!error || !error.response.data) {
-    return { status: 500, message: "Ocorreu um erro no servidor. Tente novamente."};
-  }
-  if(error.response.data.status === 401){
-    return { status: 401, message: "Você não tem autorização para acessar esses dados."};
-  }
-  if(error.response.data.errors) {
-    return { status: 400, message: error.response.data.errors}
-  }
-}
+
 
 // USUARIOS
 export const handleLogin = ({email, password, opcaoLembrar }, callback) => {
