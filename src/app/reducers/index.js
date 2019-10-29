5fb4852728c8fca5af017b30e7819d5879fbd324
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-29 09:40:33 -0300

    Produtos - Preparando actions e reducer

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -4,13 +4,15 @@ import authReducer from './auth_reducer';
 import pedidoReducer from './pedido_reducer';
 import clienteReducer from './cliente_reducers';
 import categoriaReducer from './categoria_reducer';
+import produtoReducer from './produto_reducer';
 
 
 const reducers = combineReducers({
   auth: authReducer,
   pedido: pedidoReducer,
   cliente: clienteReducer,
-  categoria: categoriaReducer
+  categoria: categoriaReducer,
+  produto: produtoReducer
 });
 
 export default reducers;
