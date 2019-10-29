deebc917d71e6b6e790ff018e23a950492020dd2
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-29 08:35:02 -0300

    Detalhes da Categoria - Criando Lista de Produtos

--- src/app/containers/Categoria/listaDeProdutos.js
+++ src/app/containers/Categoria/listaDeProdutos.js
@@ -1,59 +1,66 @@
 import React, { Component } from 'react';
 
 import Titulo from '../../components/Texto/Titulo';
-import Pesquisa from '../../components/Inputs/Pesquisa';
 import Tabela from '../../components/Tabela/Simples';
 import Paginacao from '../../components/Paginacao/Simples';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions/categorias';
+
 class ListaDeProdutos extends Component {
+
   state = {
-    pesquisa: "",
-    atual: 0
+    atual: 0,
+    limit: 5 
   }
 
+  getCategoriaProdutos(props){
+    const {atual, limit} = this.state;
+    const { usuario, categoria } = props;
+    if(!usuario || !categoria) return null;
+    this.props.getCategoriaProdutos(categoria._id, atual, limit, usuario.loja);
+  }
+
+  componentDidMount(){
+    this.getCategoriaProdutos(this.props);
+  }
+
+  componetWillUpdate(nextProps){
+    if (
+      (!this.props.usuario && nextProps.usuario) ||
+      (!this.props.categoria && nextProps.categoria)
+    ) this.getCategoriaProdutos(nextProps);
+  }
+
+  changeNumeroAtual = atual =>this.setState({ atual }, () => this.getCategoriaProdutos(this.props)); 
+
   render() {
-    const { pesquisa } = this.state;
     
-    const dados = [
-      {
-        "Produto": "Mouse 1",
-        "Estoque": 20,
-        "Disponibilidade":"sim",
-        "botaoDetalhes": "/produto/897JKHSD98"
-      },
-      {
-        "Produto": "Mouse 2",
-        "Estoque": 20,
-        "Disponibilidade":"não",
-        "botaoDetalhes": "/produto/324234fdsd"
-      },{
-        "Produto": "Mouse 3",
-        "Estoque": 20,
-        "Disponibilidade":"sim",
-        "botaoDetalhes": "/produto/234234fdsf"
-      }  
-    ]
+   const { categoriaProdutos } = this.props;
+
+   const dados = [];
+   (categoriaProdutos ? categoriaProdutos.docs : []).forEach(item => {
+     dados.push({
+       "Produto": item.titulo,
+       "SKU": item.sku,
+       "Disponibilidade": item.disponibilidade ? "Disponível" : "Indisponível",
+       "botaoDetahes": `/produto/${item._id}`
+     });
+   });
 
     return (
       <div className="ListaDeProdutos"> 
         <br/><hr/><br/>
         <Titulo tipo="h3" titulo="Produtos da Categoria" />
         <br />
-        <Pesquisa 
-          valor = { pesquisa }
-          placeholder = {"Pesquise aqui pelo nome do produto ou descrição..."}
-          onChange = { (ev) => this.onChangePesquisa(ev)}
-          onClick={() => alert('Pesquisar')}
-        />
-        <br />
         <Tabela 
-          cabecalho={["Produto", "Estoque", "Disponibilidade"]}
+          cabecalho={["Produto", "SKU", "Disponibilidade"]}
           dados={dados}
         />
         <Paginacao 
           atual={this.state.atual}
-          total={120} 
-          limite={20} 
+          total={this.props.categoriaProdutos ? this.props.categoriaProdutos.total : 0} 
+          limite={this.state.limit} 
           onClick={(numeroAtual) => this.changeNumeroAtual(numeroAtual)}
         />
       </div>
@@ -61,4 +68,10 @@ class ListaDeProdutos extends Component {
   }
 }
 
-export default ListaDeProdutos;
+const mapStateToProps = state => ({
+  categoriaProdutos: state.categoria.categoriaProdutos,
+  categoria: state.categoria.categoria,
+  usuario: state.auth.usuario
+});
+
+export default connect(mapStateToProps, actions)(ListaDeProdutos);
